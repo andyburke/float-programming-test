d549d2b1cbf6a94d7677cffc5b70b2def6721bd0
Andy Burke <aburke@bitflood.org>
2016-05-24 18:37:00 -0700

    basic

--- src/deepFinder.js
+++ src/deepFinder.js
@@ -1,5 +1,24 @@
 'use strict';
 
 module.exports = ( input, test ) => {
+    if ( typeof input !== 'object' ) {
+        return test( input ) ? [ input ] : null;
+    }
 
+    let result = null;
+
+    Object.keys( input ).forEach( key => {
+        const value = input[ key ];
+        if ( typeof value !== 'object' && test( value ) ) {
+            result = result ? result.concat( [ value ] ) : [ value ];
+        }
+        else {
+            const _result = module.exports( value, test );
+            if ( _result ) {
+                result = result ? result.concat( _result ) : _result;
+            }
+        }
+    } );
+
+    return result;
 };
--- src/finder.js
+++ src/finder.js
@@ -1,5 +1,18 @@
 'use strict';
 
 module.exports = ( input, test ) => {
+    if ( typeof input !== 'object' ) {
+        return test( input ) ? [ input ] : null;
+    }
 
+    let result = null;
+
+    Object.keys( input ).forEach( key => {
+        const value = input[ key ];
+        if ( typeof value !== 'object' && test( value ) ) {
+            result = result ? result.concat( [ value ] ) : [ value ];
+        }
+    } );
+
+    return result;
 };
